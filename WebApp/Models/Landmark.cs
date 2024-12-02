eaaea8a4db447cb22ead33a0a4b477198c8e1552
Lucianna Mendonca <luciannamend@gmail.com>
2024-12-02 17:35:13 -0500

    Edited flag on edit details page

---
+++ WebApp/Models/Landmark.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace WebApp.Models
+{
+    public class Landmark
+    {
+        public required string StateName { get; set; }
+        public required string LandmarkName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
